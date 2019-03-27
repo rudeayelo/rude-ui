e2033ea67fe6e373a7fe6ea65e919f65634c9464
Rude <rude.ayelo@xing.com>
2019-03-27 14:56:13 +0100

    chore(cleanup): Remove gatsby cache files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/rude.ayelo/Projects/rude-ui/node_modules/gatsby-mdx/gatsby-browser.js'),
-      options: {"plugins":[],"extensions":[".md",".mdx"],"mdPlugins":[[null,{"type":"yaml","marker":"-"}],null],"hastPlugins":[[null,{"root":"/Users/rude.ayelo/Projects/rude-ui"}],null],"defaultLayouts":{"default":"/Users/rude.ayelo/Projects/rude-ui/.docz/app/components/Layout.js"}},
-    }]
