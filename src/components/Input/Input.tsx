25a011c4270a5daef837d5cf29a05b5afafc7e05
Rude <rude.ayelo@xing.com>
2020-04-09 16:24:27 +0200

    fix: make size optional in Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,7 +4,7 @@ import { ElementProps, AllHTMLProps } from "../../types";
 
 type NativeInputProps = Omit<AllHTMLProps<HTMLInputElement>, "size">;
 export interface InputProps extends NativeInputProps, ElementProps {
-  size: "small" | "medium" | "large";
+  size?: "small" | "medium" | "large";
 }
 
 export const Input: React.FC<InputProps> = props => <BaseInput {...props} />;
