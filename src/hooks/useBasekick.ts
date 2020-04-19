45804b68ccd3a80836b9d6e20434b03859184c95
Rude <rude.ayelo@xing.com>
2020-04-19 16:14:18 +0200

    feat: upgrade dependencies and use `leading-trim` for the basekick

--- src/hooks/useBasekick.ts
+++ src/hooks/useBasekick.ts
@@ -1,11 +1,19 @@
 import { useTheme } from "emotion-theming";
+import { leadingTrim } from "leading-trim";
 import { ThemeTokens } from "../theme";
 import { FontSizes } from "../types";
+
+const interReference = {
+  topCrop: 7,
+  bottomCrop: 6,
+  fontSize: 40,
+  lineHeight: 1.1,
+};
 
 export const useBasekick = (size: FontSizes) => {
   const theme: ThemeTokens = useTheme();
 
-  const fontSize =
+  const scaleSize =
     size === "display" ||
     size === "page" ||
     size === "section" ||
@@ -14,36 +22,8 @@ export const useBasekick = (size: FontSizes) => {
       ? theme.fontSizes.Heading[size]
       : size;
 
-  const refTopCrop = 5;
-  const refBottomCrop = 5;
-  const referenceFontSize = 16;
-  const referenceLineHeight = 1.4;
-  const currentLineHeight = theme.lineHeights[fontSize];
-
-  const topCrop =
-    Math.max(
-      refTopCrop +
-        (currentLineHeight - referenceLineHeight) * (referenceFontSize / 2),
-      0,
-    ) / referenceFontSize;
-  const bottomCrop =
-    Math.max(
-      refBottomCrop +
-        (currentLineHeight - referenceLineHeight) * (referenceFontSize / 2),
-      0,
-    ) / referenceFontSize;
-
-  return {
-    display: "block",
-    lineHeight: fontSize,
-    "&::before, &::after": {
-      content: '""',
-      display: "block",
-      height: 0,
-      width: 0,
-      paddingTop: "1px",
-    },
-    "&::before": { marginBottom: `calc(-${topCrop}em - 1px)` },
-    "&::after": { marginTop: `calc(-${bottomCrop}em - 1px)` },
-  };
+  return leadingTrim({
+    lineHeight: theme.lineHeights[scaleSize],
+    reference: interReference,
+  });
 };
